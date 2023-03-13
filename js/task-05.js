fc777451318cd749ea218c8f273c946e9d930b0d
MargoMarm <margogerasimenko@gmail.com>
2023-03-13 20:53:08 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const inputRef = document.querySelector("#name-input");
 const outputRef = document.querySelector("#name-output");
 
 inputRef.addEventListener("input", (event) => {
-	event.currentTarget.value === ""
+	event.currentTarget.value.trim() === ""
     ? (outputRef.textContent = "Anonymous")
     : (outputRef.textContent = event.currentTarget.value);
 })
